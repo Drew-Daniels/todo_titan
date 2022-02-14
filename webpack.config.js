909a76972578c07e58cc3bdb94c07fe47247d135
Drew Daniels <drew@thedaniels.com>
2022-02-14 16:26:26 -0600

    Updated mode to production.

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,14 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: {
       index: './src/index.js',
       dom: './src/dom.js',
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Todo Titan'
+      title: 'Todo Titan',
     }),
   ],
   module: {
